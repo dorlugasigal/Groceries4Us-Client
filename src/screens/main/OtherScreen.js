7074b6f7870184bc72735b2af208d19f4eaa4525
Dor Lugasi-Gal <dorlugasigal@gmail.com>
2019-12-30 16:01:55 +0200

    :art: changing component to functional component

--- src/screens/main/OtherScreen.js
+++ src/screens/main/OtherScreen.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { AsyncStorage, Button, StatusBar, View } from 'react-native';
 import { styles } from '../../styles/main';
 import DevelopmentFlag from '../../components/DevelopmentFlag/DevelopmentFlag';
-export class OtherScreen extends React.Component {
-  static navigationOptions = {
+export function OtherScreen(props) {
+  navigationOptions = {
     title: 'Lots of features here'
   };
-  render() {
-    return (
-      <View style={styles.container}>
-        <DevelopmentFlag></DevelopmentFlag>
-        <Button title="I'm done, sign me out" onPress={this._signOutAsync} />
-        <StatusBar barStyle="default" />
-      </View>
-    );
-  }
+
   _signOutAsync = async () => {
     await AsyncStorage.clear();
-    this.props.navigation.navigate('Auth');
+    props.navigation.navigate('Auth');
   };
+
+  return (
+    <View style={styles.container}>
+      <DevelopmentFlag></DevelopmentFlag>
+      <Button title="I'm done, sign me out" onPress={_signOutAsync} />
+      <StatusBar barStyle="default" />
+    </View>
+  );
 }
