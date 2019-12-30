8c9a6249be225be7bef7734dc78ca1adc45ea653
Dor Lugasi-Gal <dorlugasigal@gmail.com>
2019-12-30 16:02:27 +0200

    :art: changing component to functional component

--- src/screens/main/HomeScreen.js
+++ src/screens/main/HomeScreen.js
@@ -4,23 +4,23 @@ import { AsyncStorage, Button, View } from 'react-native';
 import { styles } from '../../styles/main';
 import DevelopmentFlag from '../../components/DevelopmentFlag/DevelopmentFlag';
 
-export class HomeScreen extends React.Component {
-  static navigationOptions = {
+export function HomeScreen(props) {
+  navigationOptions = {
     title: 'Welcome to the app!'
   };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <DevelopmentFlag></DevelopmentFlag>
-      </View>
-    );
-  }
   _showMoreApp = () => {
-    this.props.navigation.navigate('Other');
+    props.navigation.navigate('Other');
   };
+
   _signOutAsync = async () => {
     await AsyncStorage.clear();
-    this.props.navigation.navigate('Auth');
+    props.navigation.navigate('Auth');
   };
+
+  return (
+    <View style={styles.container}>
+      <DevelopmentFlag></DevelopmentFlag>
+    </View>
+  );
 }
